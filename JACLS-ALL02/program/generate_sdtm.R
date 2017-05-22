41316957bcd897d6a328566a33717fcdb92bc943
kirimichan <mariko.ohtsuka@nnh.go.jp>
2017-05-22 16:08:16 +0900

    2017-5-22

--- JACLS-ALL02/program/generate_sdtm.R
+++ JACLS-ALL02/program/generate_sdtm.R
@@ -26,7 +26,7 @@
 # Return : dst$columnname                        #
 # ############################################## # 
 SetSEQ <- function(dst, pkey, columnname) {
-  #Init
+  # Initialize
   cntseq = 0
   save.pkey <- ""
   
@@ -38,70 +38,94 @@ SetSEQ <- function(dst, pkey, columnname) {
     dst[i,columnname] <- cntseq
     save.pkey <- dst[i,pkey]
   }
-  return(dst[,columnname])
+  return (dst[,columnname])
+  
+  # finalize
+  rm(dst)
+}
+# ############################################## # 
+# ISO8601Date function                           #
+# YYYY/MM/DD -> YYYY-MM-DD                       #
+# Arguments : dte is Date                        #
+# Return :    Date(ISO8601format)                #
+# ############################################## # 
+ISO8601Date <- function(dte) {
+  return (as.character(as.Date(dte)))
 }
 # ####################################################### # 
 # SetDmARMCD function 　                                  #
 # Set DM$ARMCD,ARM  　　　　　　　　　　                  #
 # Arguments : dst is dataframe(inputdata$確定リスク,割付) #
-# Return : resdst is dataframe(ARMCD,ARM)                 #
+# Return : resdst is dataframe(ARMCD,ARM,ACTARMCD,ACTARM) #
 # ####################################################### # 
 SetDmARMCD <- function(dst) {
+  
   # Initialize
   armcd <- NA
   arm <- NA
-  resdst <- data.frame(armcd,arm)
-  
-  # Set search table
-  search1.risk <- c("ER","F","T","HR","SR",NA)
-  search1.armcd <- c("ER-02","F-02","T-02","SR-02","HR-02","")
-  search1.arm <- c("ER-02","F-02","T-02","SR-02","HR-02","")
-  search1.T <- data.frame(risk=search1.risk,armcd=search1.armcd,arm=search1.arm)
-  indf.armcd <- "INDFAIL"
-  indf.arm <- "Induction Failure"
-  indf.T <- data.frame(armcd=indf.armcd,arm=indf.arm)
-  other.armcd <- "U"
-  other.arm <- "Unknown"
-  other.T <- data.frame(armcd=other.armcd,arm=other.arm)
+  actarmcd <- NA
+  actarm <- NA
+  resdst <- data.frame(armcd, arm, actarmcd, actarm)
   
   # Main
-  for (i in 1:nrow(dst)){
-    wk.risk <- dst[i,"risk"]
-    wk.alloc <- dst[i,"alloc"]
-    # When wk.risk is not exist in search1.T$risk, arm.T is set to NA.
-    arm.T <- search1.T[search1.T$risk==wk.risk,]
-    arm.T <- arm.T[1,]  # When NA is set, multiple row
-    # wk.risk is exist in search1.T$risk
-    if(!is.na(arm.T$armcd)){
-      if (is.na(wk.risk)){
-        # risk is NA : "Induction Failure"
-        arm.T <- indf.T
+  for (i in 1:nrow(dst)) {
+    # If it does not match any condition, set "Unknown".
+    armcd <- "U"
+    arm <- "Unknown"
+    actarmcd <- NA
+    actarm <- NA
+    
+    wk.risk <- dst[i, "risk"]
+    wk.alloc <- dst[i, "alloc"]
+    wk.arm.err <- dst[i,"arm.err"]
+    
+    if (is.na(wk.risk)) {
+      # "risk" is NA : "Induction Failure"
+      armcd <- "INDFAIL"
+      arm <- "Induction Failure"
+    }
+    else if (wk.risk == "ER" || wk.risk == "F" || wk.risk == "T") {
+      armcd <- paste(wk.risk, "-02", sep="")
+      arm <- armcd
+    }
+    else if (wk.risk == "HR" || wk.risk == "SR") {
+      if (is.na(wk.alloc)) {
+        # "allocations" is blank : "Induction Failure"
+        armcd <- "INDFAIL"
+        arm <- "Induction Failure"
       }
-      else if(wk.risk == "HR" || wk.risk == "SR"){
-        # wk.alloc is blank : "Induction Failure"
-        # wk.alloc is not "A" or "B" : "Unknown"
-        if (is.na(wk.alloc)){
-          arm.T <- indf.T
-        }
-        else if(wk.alloc == "A" || wk.alloc == "B"){
-          arm.T$armcd <- paste(arm.T$armcd, wk.alloc, sep="")
-          arm.T$arm <- paste(arm.T$arm, wk.alloc, sep="")
-        }
-        else{
-          # Unknown
-          arm.T <- other.T
+      else if (wk.alloc == "A" || wk.alloc == "B") {
+        # Replace "SR" <-> "HR"
+        ifelse(wk.risk == "HR", rep.risk <- "SR", rep.risk <- "HR")
+        armcd <- paste(rep.risk, "-02", wk.alloc, sep="")
+        arm <- armcd
+        if (!is.na(wk.arm.err)) {
+          # arm error
+          if (wk.arm.err == "割付けBだがAで治療" && wk.alloc == "B") {
+            actarmcd <- gsub("B$", "A", armcd)
+            actarm <- actarmcd
+          }
         }
       }
     }
-    # wk.risk is not exist in search1.T$risk : UnKnown
-    else{
-      arm.T <- other.T
+      
+    if (is.na(actarmcd)) {
+      # If "arm" has no error, "actarmcd and actarm" is set 
+      # same value as "armcd and arm". 
+      actarmcd <- armcd
+      actarm <- arm
     }
-    resdst[i,"armcd"] <- as.character(arm.T$armcd)
-    resdst[i,"arm"] <- as.character(arm.T$arm)
+    resdst[i, "armcd"] <- armcd
+    resdst[i, "arm"] <- arm
+    resdst[i, "actarmcd"] <- actarmcd
+    resdst[i, "actarm"] <- actarm
   }
-  return(resdst)
+  return (resdst)
+  
+  # finalize
+  rm(dst)
 }
+
 # ######################################################### # 
 # SetCeCEDECOD function 　                                  #
 # Set CE$CEDECOD,CEDTC  　　　　　　　　　　                #
@@ -110,30 +134,43 @@ SetDmARMCD <- function(dst) {
 # Return : resdst is dataframe(CEDECOD,CEDTC)               #
 # ######################################################### # 
 SetCeCEDECOD <- function(dst) {
+  
   # Initialize
   cedecod <- NA
   cedtc <- NA
-  resdst <- data.frame(cedecod,cedtc)
+  resdst <- data.frame(cedecod, cedtc)
+  
   # Main
-  for (i in 1:nrow(dst)){
-    wk.sec.ca <- dst[i,"sec.ca.date"]
-    wk.discontinuing <- dst[i,"discontinuing"]
+  for (i in 1:nrow(dst)) {
+    # If it does not match any condition, set NA
+    cedecod <- NA
+    cedtc <- NA
+    wk.sec.ca <- dst[i, "sec.ca.date"]
+    wk.discontinuing <- dst[i, "discontinuing"]
     
-    #二次がん日が空白でなければ「SECONDARY CANCER」二次がん日をセット
-    if(!is.na(wk.sec.ca)){
-      resdst[i,"cedecod"] <- "SECONDARY CANCER"
-      resdst[i,"cedtc"] <- as.character(as.Date(wk.sec.ca))
+    if (!is.na(wk.sec.ca)) {
+      # "date of SECONDARY CANCER" is not blank
+      # : set "SECONDARY CANCER", "date of SECONDARY CANCER"
+      cedecod <- "SECONDARY CANCER"
+      cedtc <- as.character(as.Date(wk.sec.ca))
     }
     
-    #中止理由が*再発*なら「DISEASE RELAPSE」再発日をセット
-    if(!is.na(wk.discontinuing)){
-      if(regexpr("再発",wk.discontinuing)>0){
-        resdst[i,"cedecod"] <- "DISEASE RELAPSE"
-        resdst[i,"cedtc"] <- as.character(as.Date(dst[i,"relapse.date"]))
+    if (!is.na(wk.discontinuing)) {
+      if (regexpr("再発", wk.discontinuing) > 0) {
+        # "discontinuing" is "RELAPSE" 
+        # : set "DISEASE RELAPSE", "date of relapse"
+        cedecod <- "DISEASE RELAPSE"
+        cedtc <- as.character(as.Date(dst[i,"relapse.date"]))
       }
     }
+
+    resdst[i,"cedecod"] <- cedecod
+    resdst[i,"cedtc"] <- cedtc
   }
-  return(resdst)
+  return (resdst)
+  
+  # finalize
+  rm(resdst)
 }
 # ######################################################### # 
 # SetDsDSDECOD function 　                                  #
@@ -143,48 +180,61 @@ SetCeCEDECOD <- function(dst) {
 # Return : resdst is dataframe(DSDECOD,DSDTC)               #
 # ######################################################### # 
 SetDsDSDECOD <- function(dst) {
+  
   # Initialize
   dsdecod <- NA
   dsdtc <- NA
-  resdst <- data.frame(dsdecod,dsdtc)
-  # Set grep table
-  grep1.discontinuing <- c("有害事象のため","死亡","プロトコール違反","不適格性","担当医により中止","治療リスクF群")
-  grep1.decod.str <- c("ADVERSE EVENT","DEATH","PROTOCOL DEVIATION","PROTOCOL DEVIATION","PHYSICIAN DECISION","PHYSICIAN DECISION")
-  grep1.dtc.clm.nm <- c("discontinuing.date","death.date","discontinuing.date","discontinuing.date","discontinuing.date","discontinuing.date")
-  grep1.T <- data.frame(discontinuing=grep1.discontinuing,dsdecod=grep1.decod.str,dsdtc=grep1.dtc.clm.nm)
-
-    # Main
-  for (i in 1:nrow(dst)){
-    wk.discontinuing <- dst[i,"discontinuing"]
-    wk.endtype <- dst[i,"endtype"]
+  resdst <- data.frame(dsdecod, dsdtc)
+  
+  # Pattern matching table
+  grep1.discontinuing <- c("有害事象のため", "死亡", "プロトコール違反", "不適格性", "担当医により中止", "治療リスクF群")
+  grep1.decod.str <- c("ADVERSE EVENT", "DEATH", "PROTOCOL DEVIATION", "PROTOCOL DEVIATION", "PHYSICIAN DECISION", "PHYSICIAN DECISION")
+  grep1.dtc.clm.nm <- c("discontinuing.date", "death.date", "discontinuing.date", "discontinuing.date", "discontinuing.date", "discontinuing.date")
+  grep1.T <- data.frame(discontinuing=grep1.discontinuing, dsdecod=grep1.decod.str, dsdtc=grep1.dtc.clm.nm)
+  # type : factor -> character
+  grep1.T$discontinuing <- as.character(grep1.T$discontinuing)
+  grep1.T$dsdecod <- as.character(grep1.T$dsdecod)
+  grep1.T$dsdtc <- as.character(grep1.T$dsdtc)
+  
+  # Main
+  for (i in 1:nrow(dst)) {
+    # If it does not match any condition, set "OTHER"
+    dsdecod <- "OTHER"
+    dsdtc <- ISO8601Date(dst[i, "discontinuing.date"])
+    wk.discontinuing <- as.character(dst[i, "discontinuing"])
+    wk.endtype <- dst[i, "endtype"]
     
-    # 終了種別が完了ならDSDECOD = ""COMPLETED"、最終確認日
-    if(!is.na(wk.endtype)){
-      if(wk.endtype == "完了"){
-        resdst[i,"dsdecod"] <- "COMPLETED"
-        resdst[i,"dsdtc"] <- as.character(as.Date(dst[i,"last.cfm.date"]))
+    if (!is.na(wk.endtype)) {
+      if (wk.endtype == "完了") {
+        # Endtype is Completion : set "COMPLETED", 
+        # date last confirmed
+        dsdecod <- "COMPLETED"
+        dsdtc <- ISO8601Date(dst[i,"last.cfm.date"])
       }
-      else{
-        if(!is.na(wk.discontinuing)){
-          for (j in 1:nrow(grep1.T)){
-            wk.grep <- regexpr(as.character(grep1.T[j,"discontinuing"]),as.character(wk.discontinuing))
-            if (wk.grep > 0){ 
-              resdst[i,"dsdecod"] <- as.character(grep1.T[j,"dsdecod"])
-              wk.clm <- as.character(grep1.T[j,"dsdtc"])
-              resdst[i,"dsdtc"] <- as.character(as.Date(dst[i,wk.clm]))
+      else {
+        # Endtype is not Completion
+        if (!is.na(wk.discontinuing)) {
+          # Search Pattern matching table
+          for (j in 1:nrow(grep1.T)) {
+            wk.grep <- regexpr(grep1.T[j,"discontinuing"], wk.discontinuing)
+            if (wk.grep > 0) { 
+              dsdecod <- grep1.T[j, "dsdecod"]
+              # get columnsname
+              wk.clm <- grep1.T[j, "dsdtc"]
+              dsdtc <- ISO8601Date(dst[i, wk.clm])
               break
             }
           }
         }
       }
     }
-    #どれにもあてはまらなければother,中止日
-    if (is.na(resdst[i,"dsdecod"])){
-      resdst[i,"dsdecod"] <- "OTHER"
-      resdst[i,"dsdtc"] <- as.character(as.Date(dst[i,"discontinuing.date"]))
-    }
+
+    resdst[i, "dsdecod"] <- dsdecod
+    resdst[i, "dsdtc"] <- dsdtc
   }
-  return(resdst)
+  return (resdst)
+  # finalize
+  rm(resdst)
 }
 # #################
 
@@ -199,27 +249,28 @@ rawdata <- read.csv(filenames[1], as.is = T, na.strings = c(""), fileEncoding = 
 setwd("../..")
 
 # Format data
-dataset <- rawdata[!is.na(rawdata[,3]), ]
-# rawdataに列名がなかったのでセット
+dataset <- rawdata[!is.na(rawdata[, 3]), ]
+# rawdataに列名がなかったのでセット,不要なら削除
 colnames(dataset) <- c("VAR1", "アーム間違い", "ALL02.No", "VAR4", "VAR5", "確定リスク", "寛解判定", "割付", "診断年月日", "診断時年齢", "VAR11", "性別", "VAR13", "VAR14", "induction.therapy開始日", "VAR16", "VAR17", "VAR18", "VAR19", "VAR20", "VAR21", "終了種別", "VAR23", "VAR24", "VAR25", "中止日", "中止理由", "VAR28", "VAR29", "最終確認日", "VAR31", "VAR32", "VAR33", "再発日", "VAR35", "二次がん日", "VAR37", "生死日", "VAR39", "VAR40", "VAR41", "VAR42", "VAR43", "VAR44", "VAR45", "VAR46", "VAR47", "VAR48", "VAR49", "VAR50", "VAR51", "VAR52", "VAR53", "VAR54", "VAR55", "VAR56", "VAR57", "VAR58", "VAR59")
 dm <- data.frame(STUDYID = rep(kStudyId, nrow(dataset)))
 dm$DOMAIN <- "DM" 
-all02.No <- formatC(dataset$ALL02.No, width = 3, flag = "0")
+all02.No <- formatC(dataset$ALL02.No, width = 4, flag = "0")
 dm$USUBJID <- paste(kStudyId, all02.No, sep = "-")
 dm$SUBJID <- all02.No
 dm$RFSTDTC <- NA
 dm$BRTHDTC <- NA
 dm$AGE <- dataset$診断時年齢
 dm$AGEU <- "YEARS"
-dm$SEX <- dataset$性別
-wk.dm <- data.frame(risk=dataset$確定リスク,alloc=dataset$割付)
+dm$SEX <- ifelse(dataset$性別 == 0, "M",
+          ifelse(dataset$性別 == 1, "F", "U"))
+wk.dm <- data.frame(risk=dataset$確定リスク, alloc=dataset$割付, arm.err=dataset$アーム間違い)
 wk.arm.T <- SetDmARMCD(wk.dm)
 dm$ARMCD <- wk.arm.T$armcd
 dm$ARM <- wk.arm.T$arm
-dm$ACTARMCD <- dm$ARMCD
-dm$ACTARM <- dm$ARM
+dm$ACTARMCD <- wk.arm.T$actarmcd
+dm$ACTARM <- wk.arm.T$actarm
 dm$COUNTRY <- "JPN"
-dm$RFXSTDTC <- as.character(as.Date(dataset$induction.therapy開始日))  # ISO8601format
+dm$RFXSTDTC <- ISO8601Date(dataset$induction.therapy開始日)  # ISO8601format
 dm$RFICDTC <- NA
 
 # ####### CE #######  
@@ -227,14 +278,14 @@ ce <- data.frame(STUDYID = rep(kStudyId, nrow(dataset)))
 ce$DOMAIN <- "CE"
 ce$USUBJID <- paste(kStudyId, dataset$検体ID, sep = "-")
 ce$CESEQ <- NA
-wk.ce <- data.frame(discontinuing=dataset$中止理由,sec.ca.date=dataset$二次がん日,relapse.date=dataset$再発日)
+wk.ce <- data.frame(discontinuing=dataset$中止理由, sec.ca.date=dataset$二次がん日, relapse.date=dataset$再発日)
 wk.cedecod.T <- SetCeCEDECOD(wk.ce)
 ce$CETERM <-wk.cedecod.T$cedecod
 ce$CEDECOD <- ce$CETERM
 ce$CEDTC <- wk.cedecod.T$cedtc
 # Sort(asc) KEY=USUBJID,CETERM,CEDTC
 ceSortlist <- order(ce$USUBJID, ce$CETERM, ce$CEDTC)
-ce <- ce[ceSortlist,]
+ce <- ce[ceSortlist, ]
 # Set CESEQ  Serial number for each USUBJID
 ce$CESEQ = SetSEQ(ce, "USUBJID", "CESEQ")
 
@@ -243,7 +294,7 @@ ds <- data.frame(STUDYID = rep(kStudyId, nrow(dataset)))
 ds$DOMAIN <- "DS"
 ds$USUBJID <- paste(kStudyId, dataset$検体ID, sep = "-")
 ds$DSSEQ <- NA
-wk.ds <- data.frame(discontinuing=dataset$中止理由,endtype=dataset$終了種別,last.cfm.date=dataset$最終確認日,discontinuing.date=dataset$中止日,death.date=dataset$生死日)
+wk.ds <- data.frame(discontinuing=dataset$中止理由, endtype=dataset$終了種別, last.cfm.date=dataset$最終確認日, discontinuing.date=dataset$中止日, death.date=dataset$生死日)
 wk.dsdecod.T <- SetDsDSDECOD(wk.ds)
 ds$DSTERM <- wk.dsdecod.T$dsdecod 
 ds$DSDECOD <- ds$DSTERM
@@ -251,7 +302,7 @@ ds$DSCAT <- "DISPOSITION EVENT"
 ds$DSSTDTC <- wk.dsdecod.T$dsdtc  # ISO8601format
 # Sort(asc) KEY=USUBJID,DSTERM,DSSTDTC
 dssortlist <- order(ds$USUBJID, ds$DSTERM, ds$DSSTDTC)
-ds <- ds[dssortlist,]
+ds <- ds[dssortlist, ]
 # Set DSSEQ  Serial number for each USUBJID
 ds$DSSEQ = SetSEQ(ds, "USUBJID", "DSSEQ")
 
@@ -261,17 +312,32 @@ mh$DOMAIN <- "MH"
 mh$USUBJID <- paste(kStudyId, dataset$検体ID, sep = "-")
 mh$MHSEQ <- NA
 mh$MHCAT <- "PRIMARY DIAGNOSIS"
-mh$MHTERM <- "急性骨髄性白血病"
+mh$MHTERM <- "小児急性リンパ性白血病"
 mh$MHDECOD <- mh$MHTERM
-mh$MHSTDTC <- NA
-mh$MHPTCD <- 20058373
-mh$MHSOC <- mh$MHTERM
-mh$MHSOCCD <- "C92.0"
-# Sort(asc) KEY=USUBJID  *MHTERM,MHSTDTC -> constant
+mh$MHSTDTC <- ISO8601Date(dataset$診断年月日)  # ISO8601format
+mh$MHPTCD <- 20064495
+mh$MHSOC <- "急性リンパ芽球性白血病"
+mh$MHSOCCD <- "C91.0"
+# Sort(asc) KEY=USUBJID
 mhsortlist <- order(mh$USUBJID)
 mh <- mh[mhsortlist,]
 # Set MHSEQ  Serial number for each USUBJID
 mh$MHSEQ = SetSEQ(mh, "USUBJID", "MHSEQ")
+
+# ####### SC #######  
+sc <- data.frame(STUDYID = rep(kStudyId, nrow(dataset)))
+sc$DOMAIN <- "SC"
+sc$USUBJID <- paste(kStudyId, dataset$検体ID, sep = "-")
+sc$SCSEQ <- NA
+sc$SCTESTCD <- "DEFRISK"
+sc$SCTEST <- "Definite Risk"
+sc$SCORRES <- dataset$確定リスク
+sc$SCSTRESC <- sc$SCORRES
+# Sort(asc) KEY=USUBJID  
+scsortlist <- order(sc$USUBJID)
+sc <- sc[scsortlist,]
+# Set RSSEQ  Serial number for each USUBJID
+sc$SCSEQ = SetSEQ(sc, "USUBJID", "SCSEQ")
 
 # ####### RS #######  
 rs <- data.frame(STUDYID = rep(kStudyId, nrow(dataset)))
@@ -281,8 +347,8 @@ rs$RSSEQ <- NA
 rs$RSTESTCD <- "CLINRESP"
 rs$RSTEST <- "Clinical Response"
 rs$RSCAT <- "DEFINED BY PROTOCOL"
-rs$RSORRES <- ifelse(dataset$CR == "CR", "CR",
-              ifelse(dataset$CR == "nonCR", "NR", "NE"))
+rs$RSORRES <- ifelse(dataset$寛解判定 == "有", "CR",
+              ifelse(dataset$寛解判定 == "無", "NR", "NE"))
 rs$RSSTRESC <- rs$RSORRES
 # Sort(asc) KEY=USUBJID  
 rssortlist <- order(rs$USUBJID)
@@ -298,3 +364,6 @@ write.csv(ds, "ds.csv", row.names = F, na = "")
 write.csv(mh, "mh.csv", row.names = F, na = "")
 write.csv(rs, "rs.csv", row.names = F, na = "")
 setwd("../../..")
+
+# finalize
+rm(list=ls())
